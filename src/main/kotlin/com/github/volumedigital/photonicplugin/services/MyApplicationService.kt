ed2012cd4b42b414523e7a370e0e939b63d2860e
GitHub Action <action@github.com>
2021-04-09 22:12:58 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/volumedigital/photonicplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.volumedigital.photonicplugin.services
+
+import com.github.volumedigital.photonicplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
